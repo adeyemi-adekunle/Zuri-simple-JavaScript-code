bad8bc4120b66868b9813dc5f25f7c917c358256
Adeyemi Adekunle <bamidek@gmail.com>
2022-07-03 11:04:20 +0100

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+// This program is to print out my name, height and  my country
+let fullName = prompt("Enter your Full Name:") 
+// console.log(fullName)
+let height = prompt("What is your height in (ft):")
+// console.log(height)
+let country = prompt("Which Country are you from?:")
+// console.log(country)
+
+alert("Your Name is: " + fullName + 
+      "\nYour Height is: " + height +
+      "\nYour Country is: " +country
+)
